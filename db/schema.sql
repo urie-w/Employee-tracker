337808ce4269038c0f1acaacaead3f3267df10bd
urie-w <urieasia@gmail.com>
2024-09-02 15:53:30 -0600

    schema nd db sql files

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+  id INTEGER SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
